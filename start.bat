e59f83b71ee869682fb076a811f0e9a6a34e0f8d
elyastctch-collab <elyastctch@gmail.com>
2026-06-19 10:46:02 +0700

    Update start.bat: Add auto-open browser feature

--- start.bat
+++ start.bat
@@ -4,5 +4,6 @@ echo ====================================================
 echo Starting Celia OC Control Dashboard...
 echo Address: http://127.0.0.1:18790
 echo ====================================================
+start /b powershell -Command "Start-Sleep -Seconds 1.5; Start-Process 'http://127.0.0.1:18790'"
 node dashboard/server.js
-pause
+
